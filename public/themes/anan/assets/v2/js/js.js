fcefa9489061336c159e040289ab54e88c92061b
Dang Jinner <nguyenhaidangyq@gmail.com>
2024-04-15 14:39:02 +0700

    update

--- public/themes/anan/assets/v2/js/js.js
+++ public/themes/anan/assets/v2/js/js.js
@@ -30,7 +30,7 @@ $(document).ready(function(){
         dots: false,
         breakpoint: 480,
         settings: {
-          slidesToShow: 1,
+          slidesToShow: 2,
           slidesToScroll: 1
         }
       }
