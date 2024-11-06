c3df6614e752d1d6007c6325b5ecdda3e679c0d7
Jinner Nguyen <dangjinner.com@gmail.com>
2024-11-06 15:30:14 +0700

    Add Autolink module

--- Modules/AutoLink/Resources/views/admin/auto_links/tabs/general.blade.php
+++ Modules/AutoLink/Resources/views/admin/auto_links/tabs/general.blade.php
@@ -4,7 +4,7 @@
         {{ Form::text('target_url', trans('autolink::attributes.target_url'), $errors, $autoLink, ['labelCol' => 2, 'required' => true]) }}
         {{ Form::select('target_type', trans('autolink::attributes.target_type'), $errors, $targetTypes, $autoLink, ['labelCol' => 2, 'required' => true]) }}
         {{ Form::checkbox('for_post', trans('autolink::attributes.for_post'), trans('autolink::auto_links.form.enable_for_post'), $errors, $autoLink, ['labelCol' => 2]) }}
-        {{ Form::checkbox('for_page', trans('autolink::attributes.for_page'), trans('autolink::auto_links.form.enable_for_page'), $errors, $autoLink, ['labelCol' => 2]) }}
+{{--        {{ Form::checkbox('for_page', trans('autolink::attributes.for_page'), trans('autolink::auto_links.form.enable_for_page'), $errors, $autoLink, ['labelCol' => 2]) }}--}}
         {{ Form::checkbox('is_duplicate', trans('autolink::attributes.is_duplicate'), trans('autolink::auto_links.form.allow_duplicate'), $errors, $autoLink, ['labelCol' => 2]) }}
 {{--        {{ Form::checkbox('is_override', trans('autolink::attributes.is_override'), trans('autolink::auto_links.form.override_old_link'), $errors, $autoLink, ['labelCol' => 2]) }}--}}
         {{ Form::checkbox('is_active', trans('autolink::attributes.is_active'), trans('autolink::auto_links.form.enable_the_auto_link'), $errors, $autoLink, ['labelCol' => 2]) }}
