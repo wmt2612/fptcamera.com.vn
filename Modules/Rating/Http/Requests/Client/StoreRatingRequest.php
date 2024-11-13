be64aca2a2f33ca3a6ef5729c56b6c545c5438f7
Jinner Nguyen <dangjinner.com@gmail.com>
2024-11-13 17:41:18 +0700

    New Feature: Review product

--- Modules/Rating/Http/Requests/Client/StoreRatingRequest.php
+++ Modules/Rating/Http/Requests/Client/StoreRatingRequest.php
@@ -6,6 +6,17 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreRatingRequest extends FormRequest
 {
+    protected function prepareForValidation()
+    {
+        $user = $this->user();
+
+        if ($user) {
+            $this->merge([
+                'user_id' => $user->id,
+            ]);
+        }
+    }
+
     /**
      * Get the validation rules that apply to the request.
      *
@@ -14,15 +25,16 @@ class StoreRatingRequest extends FormRequest
     public function rules()
     {
         return [
+            'user_id' => 'nullable|integer|exists:users,id',
             'review' => 'required|string',
             'rating' => 'required|int|in:1,2,3,4,5',
             'type' => 'required|int|in:1,2,3',
             'url' => 'required_if:type,1|string',
             'post_id' => 'required_if:type,2|int',
-            'customer_gender' => 'required|string|in:male,female',
-            'customer_name' => 'required|string',
-            'customer_email' => 'required|email',
-            'customer_phone' => 'required|string',
+            'customer_gender' => 'required_without:user_id|string|in:male,female',
+            'customer_name' => 'required_without:user_id|string',
+            'customer_email' => 'required_without:user_id|email',
+            'customer_phone' => 'required_without:user_id|string',
         ];
     }
 
